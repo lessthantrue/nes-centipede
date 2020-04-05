3fc18d343aa1e6a2417ab4348d33d16b2ae463e2
lessthantrue <nickm@outofthisworld.net>
2020-04-05 00:07:24 -0700

    now 5 points for destroyed poison mushroom

--- src/game/arrow.s
+++ src/game/arrow.s
@@ -72,29 +72,36 @@ SPEED = 5 ; velocity in px/frame (everything will work as long as this is less t
 .proc arrow_collide
     lda #ARROW_FLAG_ACTIVE
     bit arrow_f
-    beq done_collision
+    beq no_collision
         call_with_args board_convert_sprite_xy, arrow_x, arrow_y
         jsr board_xy_to_addr
         jsr board_xy_to_nametable
         jsr board_get_value
         cmp #0
-        beq done_collision ; no mushroom -> no collision
+        beq no_collision ; no mushroom -> no collision
         ; collision -> "destroy" arrow, "damage" mushroom
         sub #1
         pha ; manual argument 1: new mushroom growth level
         and #$0F ; ignore poisoned bit for this next part
-        bne :+ ; increment score if the mushroom was completely destroyed
-            statusbar_add_score MUSHROOM_SCORE
+        bne mushroom_not_destroyed ; increment score if the mushroom was completely destroyed
+            jsr board_get_value
+            and #MUSHROOM_POISON_FLAG
+            beq :+
+                statusbar_add_score POISON_MUSHROOM_SCORE
+                jmp :++
+            :
+                statusbar_add_score MUSHROOM_SCORE
+            :
             call_with_args board_set_value, #0
             pla ; get rid of that extra variable we had
-            jmp :++
-        :
+            jmp done_collision
+        mushroom_not_destroyed:
             pla
             call_with_args board_set_value, a
-        :
+        done_collision:
 
         jsr arrow_del
-    done_collision:
+    no_collision:
     rts
 .endproc
 
